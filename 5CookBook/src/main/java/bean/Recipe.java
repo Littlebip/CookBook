23e10102b35915ad237fce793206d54e18bd1b35
Lucile <littlebip.lg@hotmail.fr>
2024-02-26 15:26:02 +0100

    created first version of model ingredient and recipe

---
+++ 5CookBook/src/main/java/bean/Recipe.java
@@ -0,0 +1,67 @@
+package bean;
+
+public class Recipe {
+	private int id;
+	private String title;
+	private String description;
+	private int recipeKcal;
+
+	// constructors
+	public Recipe() {
+		super();
+	}
+
+	public Recipe(String title, String description, int recipeKcal) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.recipeKcal = recipeKcal;
+	}
+
+	public Recipe(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+
+	// methods
+	public void calculateRecipeKcal() {
+		// TODO
+	}
+
+	// setters and getters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getRecipeKcal() {
+		return recipeKcal;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Recipe [id=" + id + ", title=" + title + ", description=" + description + ", recipeKcal=" + recipeKcal
+				+ "]";
+	}
+}
